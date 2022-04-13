4480dc5a8f6ea6451860f7d2fd06d170ecd8d8fd
zarzouram <muhammadzarzoura@gmail.com>
2022-04-13 23:50:24 +0200

    workin on lstmer

--- scripts/models/lstmer.py
+++ scripts/models/lstmer.py
@@ -1,3 +1,5 @@
+from dgl import DGLGraph
+
 import torch
 import torch.nn as nn
 
@@ -82,13 +84,15 @@ class LSTMER(nn.Module):
                                          nn.Tanh(), nn.Dropout(dropout),
                                          nn.Linear(hp_size, rel_num))
 
-    def forward(self, batch):
-        seq, es_idx, pos, dep, lens = batch[1:6]
+    def forward(self, batch, es_tag):
+        seq, ents, pos, dep, lens = batch[1:6]
         rellabels, rel_dir = batch[-2:]
+
+        bsz = seq.size(0)
 
         seq, pos = self.embeddings(seq, pos)
         seq_encode_input = torch.cat((seq, pos), dim=-1)
-        seq_encoded = self.seq_encoder(seq_encode_input,
+        seq_encoded = self.seq_encoder(seq_encode_input, lens,
                                        self.padding_values[0])
 
         if self.detect_entities:
@@ -99,15 +103,56 @@ class LSTMER(nn.Module):
             edl_logits, edl_preds = self.entity_detection(seq, epsilon)
 
             if self.pretrain_ents_detection:
-                return edl_logits
+                return edl_logits, edl_preds, None, None
 
         dep = self.dep_embeddings(dep)
-        if self.tree_type == "shortest_path":
-            pass
+
+        if es_tag is None:
+            # BIOUL decode, get entity tokens indices from entity predictions
+            # construct pair candidates, get dependency tree
+            # tree, idx0, tokens_idx, e1_idx, e2_idx
+            raise NotImplementedError
         else:
-            pass
+            # get entity tokens indices
+            e1_idx = torch.nonzero(ents == es_tag[0], as_tuple=True)
+            e2_idx = torch.nonzero(ents == es_tag[1], as_tuple=True)
+            # get tree
+            if self.tree_type == "shortest_path":
+                tree = batch[7]  # type: DGLGraph
+                num_nodes = tree._batch_num_nodes["_N"].cpu()
+                idx0 = torch.repeat_interleave(torch.arange(bsz), num_nodes)
+                tokens_idx = tree.ndata["ID"]
+            elif self.tree_type == "full_tree":
+                tree = batch[5]  # type: DGLGraph
+                idx0 = slice(None)
+                tokens_idx = slice(None)
+            elif self.tree_type == "sub_tree":
+                # TODO: implement subtree
+                raise NotImplementedError
+            else:
+                raise ValueError(
+                    "tree_type value should be either 'shortest_path' or 'full_tree' or 'sub_tree'"  # noqa: E501
+                )
 
-        tree_input = torch.cat((seq_encoded, dep), dim=-1)
+        tree_inputs = [seq_encoded[idx0, tokens_idx], dep[idx0, tokens_idx]]
         if self.entity_detection:
-            tree_input = torch.cat((tree_input, edl_preds), dim=-1)
+            tree_inputs.extend(edl_preds[idx0, tokens_idx])
+        tree_inputs = torch.cat(tree_inputs, dim=-1)
+        tree.ndata["emb"] = tree_inputs
 
+        tree_d12_logits, tree_d21_logits = self.dep_tree(tree)
+
+        # select entity
+        _, num_e1 = torch.unique(e1_idx[0], return_counts=True)
+        _, num_e2 = torch.unique(e2_idx[0], return_counts=True)
+        e1_hstate = torch.split(seq_encoded[e1_idx],
+                                split_size_or_sections=num_e1)
+        e2_hstate = torch.split(seq_encoded[e2_idx],
+                                split_size_or_sections=num_e2)
+        e1_hstate = torch.cat([h.mean(dim=0) for h in e1_hstate], dim=0)
+        e2_hstate = torch.cat([h.mean(dim=0) for h in e2_hstate], dim=0)
+
+        rel_clf_input_d12 = torch.cat((tree_d12_logits, e1_hstate, e2_hstate),
+                                      dim=-1)
+        rel_clf_input_d21 = torch.cat((tree_d21_logits, e1_hstate, e2_hstate),
+                                      dim=-1)
